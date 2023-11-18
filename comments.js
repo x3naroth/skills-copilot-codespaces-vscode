b30c42a06876e1fc5d8201de8867fad2a4c54f59
x3naroth <140962169+x3naroth@users.noreply.github.com>
2023-11-18 23:53:35 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,48 @@
+//create web serv
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+//get model
+var Comment = require('../models/Comment.js');
+
+// GET ALL COMMENTS
+router.get('/', function(req, res, next) {
+  Comment.find(function (err, comments) {
+    if (err) return next(err);
+    res.json(comments);
+  });
+});
+
+// GET SINGLE COMMENT BY ID
+router.get('/:id', function(req, res, next) {
+  Comment.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// SAVE COMMENT
+router.post('/', function(req, res, next) {
+  Comment.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// UPDATE COMMENT
+router.put('/:id', function(req, res, next) {
+  Comment.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// DELETE COMMENT
+router.delete('/:id', function(req, res, next) {
+  Comment.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+//export
+module.exports = router;
